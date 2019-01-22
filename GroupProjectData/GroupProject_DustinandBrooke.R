6f282eb8cea2f09da446ab2159b25bafa27f6136
dubrewer <dustinbrewer92@yahoo.com>
2019-01-22 18:43:33 -0500

    Added .r file to group data folder

---
+++ GroupProjectData/GroupProject_DustinandBrooke.R
@@ -0,0 +1,6 @@
+rm(list=ls())
+getwd()
+setwd("C:/Users/dusti/GitHub/QB2019_Brewer/GroupProjectData")
+options(max.print = 100000)
+BirdsandWoolly <- read.csv("hf085-01-bird.csv", sep = ',')
+BirdsandWoolly
